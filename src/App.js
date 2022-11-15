619f8d27d7301758770939ce161f94192f7e9ad4
MuradINNn <96331835+MuradINNn@users.noreply.github.com>
2022-11-15 19:31:59 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import {Routes,Route}from 'react-router-dom'
+import Mainpage from './page/Mainpage/Mainpage';
+import Contactpage from './page/Contactpage/Contactpage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Mainpage/>}/>
+        <Route path='/contact' element={<Contactpage/>}/>
+      </Routes>
     </div>
   );
 }
